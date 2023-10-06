e58a982eaaca4062c06dd0595831e3389c54f2bf
Papattarada Apithanangsiri <papattarada1643@gmail.com>
2023-10-07 02:35:26 +0800

    Fix: modify pipeline to be independent of external functions

--- app/model/model.py
+++ app/model/model.py
@@ -1,7 +1,8 @@
 import pickle
 from pathlib import Path
+import re
 
-__version__ = "0.0"
+__version__ = "0.2"
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
@@ -36,14 +37,15 @@ labels = [
 
 with open(f"{BASE_DIR}/resume_pipeline_{__version__}.pkl", "rb") as f:
     resume_prediction_model = pickle.load(f)
+
+
+def preprocess_text(raw_resume):
+    resume_processed = ' '.join(re.findall('[a-zA-Z0-9]+', raw_resume)).lower()   
+    return resume_processed 
+        
     
 def predict_pipeline(text):
     prediction_result = resume_prediction_model.predict([text])
     predicted_label = labels[prediction_result[0]]
     return predicted_label
-
-# def predict_pipeline(text):
-#     preprocessed_input_text = [text]
-#     pred = model.predict(preprocessed_input_text)
-#     return pred[0]
 
